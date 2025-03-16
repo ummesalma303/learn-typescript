509048bad8e9577ad37bced0f8d758520ad00053
ummesalma303 <ummesalma333999@gmail.com>
2025-03-16 18:51:10 +0600

    Added typescript first part

---
+++ intro/script.js
@@ -0,0 +1,7 @@
+var hello = 'hello world';
+console.log(hello);
+var user = {
+    name: String,
+    id: Number,
+};
+console.log(user);
---
+++ intro/script.ts
@@ -0,0 +1,8 @@
+let hello = 'hello world';
+console.log(hello)
+
+const user: User = {
+    name: String,
+    id: Number,
+  };
+  console.log(user)
