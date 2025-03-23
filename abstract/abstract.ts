dc2283d517ad3b201cf57467377101eef881d31c
ummesalma303 <ummesalma333999@gmail.com>
2025-03-23 19:20:12 +0600

    complete section five

--- abstract/abstract.ts
+++ abstract/abstract.ts
@@ -1,3 +1,12 @@
-// abstract class Employee {
-//     constructor(private firstName: string, private lastName: string) {}
-// }
+abstract class Employee {
+    constructor(private firstName: string, private lastName: string) {}
+    abstract getSalary(): number;
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    compensationStatement(): string {
+      return `${this.fullName} makes ${this.getSalary()} a month.`;
+    }
+  }
+
+  
